e2c4fd2e6d289be184c7725d847fcafa7d4453fb
zBushraa <z.bushraaa@gmail.com>
2025-11-05 01:02:55 +0800

    Add initial project files including Java application, settings, and README

---
+++ src/App.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    System.out.println("Academic Result  ");
+         
+        int a = 0;
+        
+        if(a>=70  &&  a<=100) {
+            System.out.println( " Congratultion You Have Got A ");
+            
+        }
+        else if (a>60 && a<=69){
+             System.out.println( " You Have Got B ");
+        }
+        else if (a>50 && a<=59){
+             System.out.println( " You Have Got C");
+            }
+        else if (a>40 && a<=49){
+             System.out.println( " Unfortunately You are FAILED");
+            }
+        
+        
+        else {
+                System.out.println( " INPUT ERROR ");
+            }
+                
+        
+        
+        
+        }
+        
+            
+        }
+    
+    
+
+
+    
+
