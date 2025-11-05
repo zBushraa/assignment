65cff526de6480b3cc0a671f9b4743fda3a3f1bc
zBushraa <z.bushraaa@gmail.com>
2025-11-05 14:29:59 +0800

    Add AcademicResult class and update Taxi fare calculation logic

--- src/AcademicResult.java
+++ src/AcademicResult.java
@@ -1,4 +1,4 @@
-public class App {
+public class AcademicResult{
     public static void main(String[] args) throws Exception {
     System.out.println("Academic Result  ");
          
--- src/Taxi.java
+++ src/Taxi.java
@@ -1,8 +1,8 @@
 public class Taxi {
     public static void main(String[] args) {
         System.out.println("Taxi service fare calculation ");
-        int time =10;  //Time in  hour 
-        double km  = 4.0;  // Distance in kilometers
+        int time =23;  //Time in  hour 
+        double km  = 8.0;  // Distance in kilometers
         double fare; 
         if ( time>=7 && time <=11) // Day time from 7AM to 11 PM 
         {
@@ -13,7 +13,7 @@ public class Taxi {
                 fare = fare+(km -4)*1.9;
 
             }
-            System.out.println(" Thank you for Choosing DIDI apps.Your Total fare is for your ride "+fare  + "YUAN");
+            System.out.println("Thank you for Choosing DIDI apps.Your Total fare is for your ride " +fare+"YUAN" + "感谢您选择滴滴出行。您的总车费即为您的行程费用" + fare + "¥");
 
 
 
@@ -21,17 +21,17 @@ public class Taxi {
         else //  All other time 
     {
         fare =11;
-        if （km >4)
+        if (km > 4)
         {
             fare= fare +(km-4)*2.2;
 
 
         }
-        Sy
-    }
+        System.out.println("Thank you for Choosing DIDI apps.Your Total fare is for your ride " +fare+"YUAN" + "感谢您选择滴滴出行。您的总车费即为您的行程费用" + fare + "¥");
 
           
 
     }
     
+}
 }
